671f470fd22d5fe719e77d5b6f23600c679f7225
IvanOnohov <ivan.onokh.july@gmail.com>
2024-11-26 21:39:45 +0900

    Добавьте файлы проекта.

---
+++ StringTravel/Program.cs
@@ -0,0 +1,32 @@
+﻿string s1 = "hight";
+string s2 = "gustion";
+string s3 = "rad";
+int result = string.Compare(s1, s2);
+if (result < 0)
+{
+    Console.WriteLine("Строка s1 перед строкой s2");
+}
+else if (result > 0)
+{
+    Console.WriteLine("Строка s1 стоит после строки s2");
+}
+else
+{
+    Console.WriteLine("Строки s1 и s2 идентичны");
+}
+char ch ='g';
+int g =s1.IndexOf(ch);
+Console.WriteLine(g);
+Console.WriteLine( s1.Equals(ch));
+Console.WriteLine(s2.Equals(ch));
+Console.WriteLine(s1.Contains(s3));
+Console.WriteLine(string.CompareOrdinal(s2,s1));
+ if (s1 == s2)
+{
+    Console.WriteLine("Строки одинаковые");
+}
+ else if(s1 != s2)
+{
+    Console.WriteLine("Строки разные");   
+}
+
